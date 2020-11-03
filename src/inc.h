530945b02e5f4a148bd5ca7dd5d540ef924a3238
Kirill Fernandez <fernandes.kir@yandex.ru>
2020-11-03 16:13:24 +0300

    Added data parser

--- src/inc.h
+++ src/inc.h
@@ -1,47 +1,46 @@
 #ifndef _inc_h_
 #define _inc_h_
 #include "stm8s_conf.h"
-#define BitMask(a,b) (((a)&(b))==(b))
+#define BitMask(a, b) (((a) & (b)) == (b))
 /*for uart recieve*/
-extern unsigned char rxCount;    /*Количество принятых байтов*/
-extern unsigned char rxData[64U];  /*Массив с принятыми данными*/
-extern unsigned char txCount;    /*Количество данных для отправки*/
-extern unsigned char txData[64U];  /*Массив с данными для отправки*/
-extern _Bool status;   /*булевская переменная для указания наличия необработанных приянтых данных*/
-extern _Bool completed;/*булевская переменная для указания статуса распознавания данных*/ 
+extern unsigned char rxCount;	  /*Количество принятых байтов*/
+extern unsigned char rxData[64U]; /*Массив с принятыми данными*/
+extern unsigned char txCount;	  /*Количество данных для отправки*/
+extern unsigned char txData[64U]; /*Массив с данными для отправки*/
+extern _Bool status;			  /*булевская переменная для указания наличия необработанных приянтых данных*/
+extern _Bool completed;			  /*булевская переменная для указания статуса распознавания данных*/
 extern _Bool resData;
 extern unsigned char temp;
 extern unsigned char cnt;
 /*for I2C init*/
 extern uint8_t address;
 /*for sig ==0x5FU*/
-extern unsigned char sig;           /*Сигнатура*/
-extern unsigned short dest_id;      /*dest id девайса*/
-extern unsigned char cmd;           /*Команда в пакете*/
-extern unsigned char sz;            /*Размер пакета с данными*/
-extern unsigned char crc_head;      /*Crc до байта с размером данных*/
-extern unsigned char crc_tail;      /*Crc данных с раздела data*/
+extern unsigned char sig;	   /*Сигнатура*/
+extern unsigned char dest_id; /*dest id девайса*/
+extern unsigned char dest_id_1;
+extern unsigned char cmd;	   /*Команда в пакете*/
+extern unsigned char sz;	   /*Размер пакета с данными*/
+extern unsigned char crc_head; /*Crc до байта с размером данных*/
+extern unsigned char crc_tail; /*Crc данных с раздела data*/
 extern unsigned char crc_head_rec;
 extern unsigned char crc_tail_rec;
-/*for sifg == 0xF6U*/
-extern unsigned char offs;
-extern unsigned char size;
-extern unsigned char name; 
-typedef struct{
-    unsigned char buffer[64U];
+typedef struct
+{
+	unsigned char buffer[64U];
 	unsigned char inp;
 	unsigned char output;
 } Ring_buff;
-extern void put(unsigned char data,Ring_buff* buffer);
+extern void put(unsigned char data, Ring_buff *buffer);
 extern unsigned char pop(Ring_buff *buff);
 extern unsigned char get_count(Ring_buff *buff);
-extern void clean(Ring_buff* buff);
-extern _Bool IsEmpty(Ring_buff* buff);
+extern void clean(Ring_buff *buff);
+extern _Bool IsEmpty(Ring_buff *buff);
 extern Ring_buff bf;
+extern Ring_buff tbf;
 /***************************************************************************************/
 void SystemInit(void);
-void recognize_data(unsigned char data);  /*Функция распознавания принятых данных*/
-uint8_t dev_addr(void);/*Функция, определяющая адрес устройства*/
+void recognize_data(unsigned char data); /*Функция распознавания принятых данных*/
+uint8_t dev_addr(void);										  /*Функция, определяющая адрес устройства*/
 void gpio_config(void);
 void I2C_recognize();
 void i2c_init(unsigned char addr);
--- src/src.c
+++ src/src.c
@@ -3,90 +3,83 @@
 unsigned char MessageBegin;
 void recognize_data(unsigned char data)
 {
-
-  if (sig == 0x5FU)
+  if (rxCount > 0)
   {
-    /************************************************/
-    if (rxCount == 0x02U)
+    if (rxCount < 6)
     {
-      dest_id = data;
-      crc_head_rec = crc_head_rec ^ data;
+      if (rxCount == 5)
+      {
+        sz = data;
+        put(data, &tbf);
+        ++rxCount;
+        return;
+      }
+      else
+      {
+        crc_head ^= data;
+        put(data, &tbf);
+        ++rxCount;
+        return;
+      }
     }
-    /**********************************************/
-    if (rxCount == 0x03U)
+    else
     {
-      dest_id += data;
-      crc_head_rec = crc_head_rec ^ data;
+      if (rxCount == sz + 7 && rxCount != 0U)
+      {
+        crc_tail = data;
+        rxCount = 0U;
+        if (crc_head == crc_head_rec)
+        {
+          if (crc_tail == crc_tail_rec)
+          {
+            GPIOA->ODR |= (1U << 1);
+          }
+          else
+          {
+            return;
+          }
+        }
+        else
+          return;
+      }
     }
-    /**********************************************/
-    if (rxCount == 0x04U)
-    {
-      cmd = data;
-      crc_head_rec = crc_head_rec ^ data;
-    }
-    /***********************************************/
-    if (rxCount == 0x05U)
-    {
-      sz = data;
-      crc_tail_rec = crc_tail_rec ^ data;
-    }
-    /************************************************/
-    if (rxCount == 0x06U)
-    {
-      crc_head = data;
-    }
-    /************************************************/
-    if ((rxCount > 0x06U) && (rxCount < sz + 6U))
-    {
-      rxData[rxCount - 0x07U] = data; /*С 7 т.к. адресация идет с нуля*/
-      crc_tail_rec = crc_tail_rec ^ data;
-    }
-    /************************************************/
-    if (rxCount == sz + 0x06U)
-    {
-      crc_tail = data;
-    }
-    if ((crc_head != 0xFFU) && (rxCount == sz + 0x07U))
-    {
-      completed = 1;
-      rxCount = 0x00U;
-    }
-    /*************************************************/
   }
-  else if (sig == 0xF6U)
+  else
   {
-    if (rxCount == 2)
+    if (data == 0x5F)
     {
-      offs = temp;
+      ++rxCount;
+      put(data, &tbf);
+      return;
     }
-    else if (rxCount == 3)
+    else
     {
-      size = temp;
+      return;
     }
-    else if (rxCount == 4)
-    {
-      name = temp;
-      completed = 1;
-    }
-    return;
   }
 }
-void put(unsigned char data, Ring_buff* buff){
-  buff->buffer[buff->inp++]=data;
-  if(buff->inp == 64U) buff->inp=0;
+void put(unsigned char data, Ring_buff *buff)
+{
+  buff->buffer[buff->inp++] = data;
+  if (buff->inp == 64U)
+    buff->inp = 0;
 }
-unsigned char pop(Ring_buff* buff){
+unsigned char pop(Ring_buff *buff)
+{
   unsigned char ret = buff->buffer[buff->output++];
-   if(buff->inp==buff->output)
+  if (buff->inp == buff->output)
   {
-    buff->inp=0x00U;
-    buff->output=0x00U;
+    buff->inp = 0x00U;
+    buff->output = 0x00U;
   }
   return ret;
 }
-_Bool IsEmpty(Ring_buff* buff){
-  if((buff->inp)==(buff->output)||buff->inp ==0U) return 1;
-  else return 0;
+_Bool IsEmpty(Ring_buff *buff)
+{
+  if ((buff->inp) == (buff->output) || buff->inp == 0U)
+    return 1;
+  else
+    return 0;
 }
 unsigned char dev_addr(void)
 {
@@ -111,32 +104,32 @@ void gpio_init(void)
 /*******************************************************************************/
 void i2c_init(unsigned char addr)
 {
-  I2C->FREQR = 16U;         //SET 2MHz CLOCKING
+  I2C->FREQR = 16U;                //SET 2MHz CLOCKING
   I2C->CR1 |= (1U << 7 | 1U << 0); //DISABLE CLOCK STRECHING AND PERIPH ENABLE
   I2C->CR2 |= (1U << 2);           //ACKNOWLEDGE ENABLE
   I2C->OARL = (address << 1);      //SET ADDRESS
-  I2C->OARH|=(1U<<6);
-  I2C->ITR	= 0x07;					      // all I2C interrupt enable
+  I2C->OARH |= (1U << 6);
+  I2C->ITR = 0x07; // all I2C interrupt enable
 }
 /*******************************************************************************/
-	void I2C_transaction_begin(void)
-	{
-		MessageBegin = TRUE;
-	}
-	void I2C_transaction_end(void)
-	{
-          
-		//Not used in this example
-	}
-	void I2C_byte_received(u8 u8_RxData)
-	{
-          put(u8_RxData, &bf);
-         // return;
-		}
-	u8 I2C_byte_write(void)
-	{
-          asm("nop");
-	}
+void I2C_transaction_begin(void)
+{
+  MessageBegin = TRUE;
+}
+void I2C_transaction_end(void)
+{
+
+  //Not used in this example
+}
+void I2C_byte_received(u8 u8_RxData)
+{
+  put(u8_RxData, &bf);
+  // return;
+}
+u8 I2C_byte_write(void)
+{
+  return txData[txCount++];
+}
 /*******************************************************************************/
 void SystemInit(void)
 {
@@ -146,107 +139,101 @@ void SystemInit(void)
   CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);       /*PRESKALER WITH CPU 1 */
   gpio_init();
   UART1_DeInit();
-  UART1_Init(9600U, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO, UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE); /*UART1 CONFIG*/
-   UART1->CR5 |= (1 << 1); // IrDA mode enable
-  UART1->PSCR = 0x01; // divede the source clock by 1 for IrDA
+  UART1_Init(9600U, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO, UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
+  UART1->CR2 |= (1U << 2);
+  UART1->CR2 |= (1U << 3);
+  /*UART1 CONFIG*/
+  UART1->CR5 |= (1 << 1); // IrDA mode enable
+  UART1->PSCR = 0x01;     // divede the source clock by 1 for IrDA
   address = dev_addr();
   I2C_DeInit();
   i2c_init(address);
-  UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE);
- enableInterrupts();
+  //UART1_ITConfig(UART1_IT_RXNE_OR, ENABLE);
+  enableInterrupts();
 }
 /*******************************************************************************/
 #ifdef USE_FULL_ASSERT
 void assert_failed(u8 *file, u32 line)
 {
-  while (1);
+  while (1)
+    ;
 }
 #endif
 /*******************************************************************************/
 INTERRUPT_HANDLER(UART1_RX_IRQHandler, 18)
 {
   ++rxCount;
-  UART1_SendData8(address);
-  if (temp == 0xF5U && rxCount == 1U)
+  temp = UART1->DR;
+  if (temp == 0x5FU && rxCount == 1U) /*if sign = 0x5f ->write data to variable sig*/
   {
-    status = 1;
-    sig = temp;
-    return;
+    status = 1; /*enable status*/
+    sig = temp; /*write signature to var. sig*/
+    return;     /*go out interrupt*/
   }
-  else if (temp == 0xF6U && rxCount == 1U)
+  else if (rxCount != 0x01U && status)
   {
-    status = 1;
-    sig = temp;
-    return;
+    rxData[rxCount] = temp; /*add data to array */
   }
   else
   {
-    if (rxCount == 1)
-    {
-      return;
-    }
-    else
-    {
-      status = 1;
-      return;
-    }
+    return; /*else ->return out interrupt*/
   }
 }
 /*******************************************************************************/
 INTERRUPT_HANDLER(I2C_IRQHandler, 19)
 {
-	static u8 sr1;					
-	static u8 sr2;
-	static u8 sr3;
-	
-// save the I2C registers configuration
-sr1 = I2C->SR1;
-sr2 = I2C->SR2;
-sr3 = I2C->SR3;
+  static u8 sr1;
+  static u8 sr2;
+  static u8 sr3;
 
-// Communication error? 
-  if (sr2 & (I2C_SR2_WUFH | I2C_SR2_OVR |I2C_SR2_ARLO |I2C_SR2_BERR))
-  {		
-    I2C->CR2|= I2C_CR2_STOP;  // stop communication - release the lines
-    I2C->SR2= 0;					    // clear all error flags
-	}
-//More bytes received ? 
+  // save the I2C registers configuration
+  sr1 = I2C->SR1;
+  sr2 = I2C->SR2;
+  sr3 = I2C->SR3;
+
+  // Communication error?
+  if (sr2 & (I2C_SR2_WUFH | I2C_SR2_OVR | I2C_SR2_ARLO | I2C_SR2_BERR))
+  {
+    I2C->CR2 |= I2C_CR2_STOP; // stop communication - release the lines
+    I2C->SR2 = 0;             // clear all error flags
+  }
+  //More bytes received ?
   if ((sr1 & (I2C_SR1_RXNE | I2C_SR1_BTF)) == (I2C_SR1_RXNE | I2C_SR1_BTF))
   {
     I2C_byte_received(I2C->DR);
   }
-// Byte received ? 
+  // Byte received ?
   if (sr1 & I2C_SR1_RXNE)
   {
     //name = I2C->DR;
     I2C_byte_received(I2C->DR);
   }
- //NAK? (=end of slave transmit comm) 
+  //NAK? (=end of slave transmit comm)
   if (sr2 & I2C_SR2_AF)
-  {	
-    I2C->SR2 &= ~I2C_SR2_AF;	  // clear AF
-		I2C_transaction_end();
-	}
-// Stop bit from Master  (= end of slave receive comm)
-  if (sr1 & I2C_SR1_STOPF) 
   {
-    I2C->CR2 |= I2C_CR2_ACK;	  // CR2 write to clear STOPF
-		I2C_transaction_end();
-	}
-// Slave address matched (= Start Comm) 
+    I2C->SR2 &= ~I2C_SR2_AF; // clear AF
+    I2C_transaction_end();
+  }
+  // Stop bit from Master  (= end of slave receive comm)
+  if (sr1 & I2C_SR1_STOPF)
+  {
+    I2C->CR2 |= I2C_CR2_ACK; // CR2 write to clear STOPF
+    I2C_transaction_end();
+  }
+  // Slave address matched (= Start Comm)
   if (sr1 & I2C_SR1_ADDR)
-  {	 
-		I2C_transaction_begin();
-	}
-// More bytes to transmit ? 
+  {
+    I2C_transaction_begin();
+  }
+  // More bytes to transmit ?
   if ((sr1 & (I2C_SR1_TXE | I2C_SR1_BTF)) == (I2C_SR1_TXE | I2C_SR1_BTF))
   {
-		I2C->DR = I2C_byte_write();
+    I2C->DR = I2C_byte_write();
   }
-// Byte to transmit ? 
+  // Byte to transmit ?
   if (sr1 & I2C_SR1_TXE)
   {
     //I2C->DR = name;
     I2C->DR = I2C_byte_write();
-  }	
+  }
 }
