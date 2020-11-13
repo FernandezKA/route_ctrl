ab9fa85bd7507265db6ecc0ecaf262e67824263a
Kirill Fernandez <fernandes.kir@yandex.ru>
2020-11-13 20:30:23 +0300

    Bug fixed

--- src/inc.h
+++ src/inc.h
@@ -4,7 +4,7 @@
 /***********************/
 #define BitMask(a, b) (((a) & (b)) == (b))
 #define BUFF_SIZE (64U)
-#define DEF_ADDR (0x34U)
+#define DEF_ADDR (0x32U)
 /***********************/
 extern bool started;
 extern unsigned char temp;
@@ -14,29 +14,66 @@ struct RING
 public:
   unsigned char inp;
   unsigned char out;
+  unsigned char count;
+  bool rollback_disable;
   unsigned char data[BUFF_SIZE];
   RING()
   {
-    inp = out = 0U;
+    inp = 0U;
+    out = 0U;
+    count = 0U;
+    rollback_disable = false;
   }
-void push(const unsigned char temp)
+  inline void push(const unsigned char temp)
   {
-    if (inp == BUFF_SIZE - 1U)
-    {
-      inp = 0U;
-    }
+    if (isFull()) return;
     data[inp++] = temp;
+    if (inp == BUFF_SIZE) inp = 0U;
+    count++;
+//    if (count == BUFF_SIZE - 1U) full = true;
   }
-unsigned char pull(void)
+  inline unsigned char pull(void)
+  {   
+    register unsigned char o1;
+
+    if (isEmpty()) { 
+      rollback_disable = true;
+      return 0;
+    }
+
+    o1 =  data[out++];
+    if (out == BUFF_SIZE) out = 0U;
+    --count;
+    return o1;
+  }
+
+  inline void rollback(void) // rolls back buffer receiver pointer one byte backward
+  {
+    if (rollback_disable) {
+      rollback_disable = false;
+      return;
+    }
+    rollback_disable = false;
+    if (out == 0) out = BUFF_SIZE;
+    out--;
+    count++;
+  }
+  
+  inline bool isEmpty(void)
+  {
+    if (count == 0) return true; else return false;
+  }
+  
+  inline bool isFull(void)
+  {
+    return (count==(BUFF_SIZE - 1U));
+  }
+
+  inline unsigned char getCount(void) 
   { 
-    if(out>inp){
-       out = inp;
-    }
-    if(out==BUFF_SIZE-1U){
-       out = inp = 0x00U;
-    }
-    return data[out++];
-  }
+    return count;
+  };
+  
 };
 extern RING uart;
 extern RING i2c;
